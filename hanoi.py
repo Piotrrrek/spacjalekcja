b6dd41c03f56b9af3d2ef72f408e609ec4f5418c
Piotrek <g.piotreg@gmail.com>
2019-11-26 11:39:19 +0100

    Create hanoi.py

---
+++ hanoi.py
@@ -0,0 +1,13 @@
+'''
+a - slupek zrodlowy
+b - docelowy
+pom - pomocniczy
+pom=6-a-b
+n - liczba krazkow
+if n==1
+    krazek nr 1 - a - > b
+else
+    n-1 krazkow   a   - > pom
+    krazek n      a   - > b
+    n-1 krazkow   pom - > b
+'''
